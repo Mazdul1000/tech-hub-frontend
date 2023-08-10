f8d762b1e11afe12f3b0396636248a93715c0b5d
mazdul1000 <mazdul1000@gmail.com>
2023-08-10 23:27:33 +0600

    completed the product detail page ui and functionality

--- src/pages/products/[...slug].js
+++ src/pages/products/[...slug].js
@@ -1,62 +1,195 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 import { useRouter } from "next/router";
-import styles from '@/styles/ProductDetail.module.css'
+import styles from "@/styles/ProductDetail.module.css";
 import Image from "next/image";
+import { Button, Col, Descriptions, Form, Input, Rate, Row } from "antd";
+import { transformString } from "@/utils/transformString";
+import { Controller, useForm } from "react-hook-form";
 
-const ProductDetails = ({product, category}) => {
-    return (
-       <div className={styles.container}>
-        <div className={styles.main}>
+const ProductDetails = ({ product, category }) => {
+  const keyFeatures = product?.key_features;
+  const { handleSubmit, control, formState } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data); // You can replace this with your desired submission logic
+  };
+
+
+
+  const items = Object.keys(keyFeatures).map((key) => ({
+    key: key,
+    label: transformString(key),
+    styles: { color: "red" },
+    children: <p key={transformString}>{keyFeatures[key]}</p>,
+  }));
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <Row gutter={[0, 40]}>
+          <Col sm={{ span: 24 }} md={{ span: 8 }}>
             <div className={styles.image_container}>
-                <Image src={product.image_url} height={400} width={400} alt={product.product_name}/>
+              <Image
+                src={product.image_url}
+                height={400}
+                width={300}
+                alt={product.product_name}
+              />
             </div>
+          </Col>
+          <Col
+            className={styles.product_info}
+            sm={{ span: 24 }}
+            md={{ span: 16 }}
+          >
+            <h2>{product.product_name}</h2>
+            <p>Price: {product.price}</p>
+            <p>Category: {transformString(product.category)}</p>
+            <p>Stock Status: {product.status}</p>
+            <div>
+              {" "}
+              <Rate
+                className={`${styles.rating}`}
+                disabled
+                allowHalf
+                defaultValue={product.rating}
+              />
+            </div>
+            <div>
+              <p>{product.description}</p>
+            </div>
+          </Col>
+          <Col sm={{ span: 24 }}>
+            <Descriptions
+              className={styles.key_features}
+              title="Key Features"
+              size="medium"
+              bordered
+              column={1}
+              items={items}
+            />
+          </Col>
+        </Row>
+      </div>
+      <div className={styles.review_section}>
+        <h3 style={{}}>Reviews :</h3>
+        <Row gutter={[0, 40]} style={{ padding:"30px"}}>
+          <Col className={`gutter-row ${styles.review_card}`} span={24}>        
+          <h3>User 1</h3>
+              <p>This product is very good. Recommeded to everyone</p>
+              <Rate
+                className={`${styles.rating}`}
+                allowHalf
+                disabled
+                defaultValue={product.rating}
+              />
+          </Col>
+          <Col className={`gutter-row ${styles.review_card}`} span={24}>        
+          <h3>User 1</h3>
+              <p>This product is very good. Recommeded to everyone</p>
+              <Rate
+                className={`${styles.rating}`}
+                allowHalf
+                disabled
+                defaultValue={product.rating}
+              />
+          </Col>
+          <Col className={`gutter-row ${styles.review_card}`} span={24}>        
+              <h3>User 1</h3>
+              <p>This product is very good. Recommeded to everyone</p>
+              <Rate
+                className={`${styles.rating}`}
+                allowHalf
+                disabled
+                defaultValue={product.rating}
+              />
+          </Col>
+        </Row>
+
+        <h3>Write a Review :</h3>
+        <div className={styles.formContainer}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label className={styles.label}>User Name</label>
+          <Controller
+            name="userName"
+            control={control}
+            defaultValue=""
+            rules={{ required: 'User name is required' }}
+            render={({ field, fieldState }) => (
+              <div>
+                <input {...field} className={styles.inputField} placeholder="Enter your user name" />
+                {fieldState.error && <p>{fieldState.error.message}</p>}
+              </div>
+            )}
+          />
         </div>
-        <div className={styles.reviews_section}>
-           <h1>Reviews section</h1>
+        <div>
+          <label className={styles.label}>Review</label>
+          <Controller
+            name="review"
+            control={control}
+            defaultValue=""
+            rules={{ required: 'Review is required' }}
+            render={({ field, fieldState }) => (
+              <div>
+                <textarea {...field} className={styles.textareaField} placeholder="Write your review here" />
+                {fieldState.error && <p>{fieldState.error.message}</p>}
+              </div>
+            )}
+          />
         </div>
-       </div>
-    );
+        <div>
+          <label className={styles.label}>Rating</label>
+          <Controller
+            name="rating"
+            control={control}
+            defaultValue={0}
+            render={({ field }) => (
+              <div className={styles.rating}>
+                <Rate {...field} allowHalf defaultValue={product.rating} />
+              </div>
+            )}
+          />
+        </div>
+        <button  type="submit" disabled={formState.isSubmitting} className={styles.submit_btn}>
+          Submit Review
+        </button>
+      </form>
+    </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
 
-
 ProductDetails.getLayout = function getLayout(page) {
-    return (
-        <RootLayout>
-            {page}
-        </RootLayout>
-    )
-}
+  return <RootLayout>{page}</RootLayout>;
+};
 
 export const getStaticPaths = async () => {
-    const res = await fetch("http://localhost:5001/products");
-    const data =await res.json();
-    const paths = data.products.map((product) => ({
-        params :{ slug: [product.category, product._id]}
-    }))
+  const res = await fetch("http://localhost:5001/products");
+  const data = await res.json();
+  const paths = data.products.map((product) => ({
+    params: { slug: [product.category, product._id] },
+  }));
 
-    return {
-        paths,
-        fallback: false
-    }
-}
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-export const getStaticProps = async ({params}) => {
-    const [category, productId] = params.slug;
-    const res = await fetch(`http://localhost:5001/product/${productId}`)
-    const data = await res.json();
+export const getStaticProps = async ({ params }) => {
+  const [category, productId] = params.slug;
+  const res = await fetch(`http://localhost:5001/product/${productId}`);
+  const data = await res.json();
 
-    return {
-        props: {
-            product: data.product,
-            category,
-        }
-    }
-}
-
-
-
-
-
-
+  return {
+    props: {
+      product: data.product,
+      category,
+    },
+  };
+};
